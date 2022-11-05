ec839e05260186b6cfa44859486b513c7fc960fb
fof002 <80104002+fof002@users.noreply.github.com>
2022-11-05 20:35:22 +0100

    fgh

--- js/nav.js
+++ js/nav.js
@@ -3,4 +3,5 @@ const mobileNav = document.querySelector("#mobile-menu");
 
 navBtn.addEventListener("click", () => {
   mobileNav.classList.toggle("active");
+  navBtn.classList.toggle("fa-xmark");
 });
