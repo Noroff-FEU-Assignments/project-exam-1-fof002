e1cadef910b6b51dc9b45dc152441a4ef68ffc76
fof002 <80104002+fof002@users.noreply.github.com>
2022-11-07 13:47:07 +0100

    API call
    Added async function in index.js

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const url = "https://frithjof.shop/test/wp-json/wp/v2/posts/";
+const blogPostContainer = document.querySelector("#blog-post-container");
+async function getBlogPosts() {
+  blogPostContainer.innerHTML = "";
+  try {
+    const response = await fetch(url);
+    const blogPosts = await response.json();
+    console.log(blogPosts);
+    for (let i = 0; i < blogPosts.length; i++) {
+      blogPostContainer.innerHTML += `  <div class="blog-post">
+      <img
+        src="https://i0.wp.com/worldadventuredivers.com/wp-content/uploads/2022/07/IMG_0633r.jpg?w=2000&ssl=1"
+        alt=""
+        width="100%"
+      />
+      <div class="content-container">
+        <section>
+          <h2>This is a heading: lorem ipsum</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+            dolor quas autem? Nemo, a quos. Non sequi nostrum tempore nemo?
+            <a href="#">Read more</a>
+          </p>
+        </section>
+        <div class="meta-container">
+          <div>Name Namesen</div>
+          <div>3. september - 1 min read</div>
+        </div>
+      </div>
+    </div>`;
+    }
+  } catch {
+    blogPostContainer.innerHTML =
+      "Something went wrong! Contact us for assitance";
+  }
+}
+getBlogPosts();
--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,5 @@
 const navBtn = document.querySelector("#mobile-navigation-btn");
 const mobileNav = document.querySelector("#mobile-menu");
-
 navBtn.addEventListener("click", () => {
   mobileNav.classList.toggle("active");
   navBtn.classList.toggle("fa-xmark");
