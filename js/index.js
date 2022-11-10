e0fad4886c2e9c31b70768a94428d80c0f61c7bc
fof002 <80104002+fof002@users.noreply.github.com>
2022-11-10 14:35:04 +0100

    got featured images

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const url = "https://frithjof.shop/test/wp-json/wp/v2/posts/";
+const url = "https://frithjof.shop/test/wp-json/wp/v2/posts?_embed";
 const blogPostContainer = document.querySelector("#blog-post-container");
 async function getBlogPosts() {
   blogPostContainer.innerHTML = `<div class="loader"></div>`;
@@ -6,33 +6,29 @@ async function getBlogPosts() {
     const response = await fetch(url);
     const blogPosts = await response.json();
     console.log(blogPosts);
+    blogPostContainer.innerHTML = "";
     for (let i = 0; i < blogPosts.length; i++) {
-      blogPostContainer.innerHTML = "";
-      blogPostContainer.innerHTML += `  <div class="blog-post">
-      <img
-        src="https://i0.wp.com/worldadventuredivers.com/wp-content/uploads/2022/07/IMG_0633r.jpg?w=2000&ssl=1"
-        alt=""
-        width="100%"
-      />
+      let post = blogPosts[i];
+      let description = post.excerpt.rendered;
+      let title = post.title.rendered;
+      let id = post.id;
+      let image = post._embedded["wp:featuredmedia"]["0"].source_url;
+
+      blogPostContainer.innerHTML += `<div class="blog-post">
       <div class="content-container">
+      <img src="${image}" alt="">
         <section>
-          <h2>This is a heading: lorem ipsum</h2>
+          <h2>${title}</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            dolor quas autem? Nemo, a quos. Non sequi nostrum tempore nemo?
-            <a href="#">Read more</a>
+            ${description}
+            <a href="blog-specific.html?id=${id}">Read more</a>
           </p>
-        </section>
-        <div class="meta-container">
-          <div>Name Namesen</div>
-          <div>3. september - 1 min read</div>
-        </div>
       </div>
     </div>`;
     }
   } catch {
     blogPostContainer.innerHTML =
-      "Something went wrong! Contact us for assitance";
+      "Something went wrong! Try again shortly. If this doesn't work, Contact us for assitance";
   }
 }
 getBlogPosts();
