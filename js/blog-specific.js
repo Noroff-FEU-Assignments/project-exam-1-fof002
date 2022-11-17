5616e242808559e00bfd421ad903764572affd3a
fof002 <80104002+fof002@users.noreply.github.com>
2022-11-17 12:54:59 +0100

    Added blog specific

--- js/blog-specific.js
+++ js/blog-specific.js
@@ -3,6 +3,7 @@ const params = new URLSearchParams(queryString);
 const queryId = params.get("id");
 const url = `https://frithjof.shop/test/wp-json/wp/v2/posts/${queryId}?_embed`;
 const main = document.querySelector("main");
+const mainTitle = document.querySelector("title");
 
 async function getPost() {
   const response = await fetch(url);
@@ -13,7 +14,7 @@ async function getPost() {
   let image = post._embedded["wp:featuredmedia"]["0"].source_url;
   let author = post._embedded["author"]["0"].name;
   let date = post.date;
-
+  mainTitle.innerHTML = title + " " + "- The Mountain Blog";
   main.innerHTML = `<div class="blog-post-specific">
         <img src="${image}" alt="">
           <section>
