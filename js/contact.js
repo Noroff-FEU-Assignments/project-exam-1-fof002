233b5ee4af92ad6be14b7e2530036e57116716e7
fof002 <80104002+fof002@users.noreply.github.com>
2022-11-18 13:37:45 +0100

    added contact page

---
+++ js/contact.js
@@ -0,0 +1,54 @@
+const email = document.querySelector("#email");
+const submitButton = document.querySelector(".submitButton");
+const emailError = document.querySelector(".emailError");
+const form = document.querySelector("#form");
+const firstName = document.querySelector("#firstname");
+const firstNameError = document.querySelector(".firstNameError");
+const lastName = document.querySelector("#lastname");
+const lastNameError = document.querySelector(".lastNameError");
+const message = document.querySelector("#message");
+const messageError = document.querySelector(".messageError");
+
+form.addEventListener("submit", validateForm);
+
+function validateForm(event) {
+  event.preventDefault();
+
+  if (validateEmail(email.value)) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+  if (checkLength(firstName, 3)) {
+    firstNameError.style.display = "block";
+  } else {
+    firstNameError.style.display = "none";
+  }
+  if (checkLength(lastName, 3)) {
+    lastNameError.style.display = "block";
+  } else {
+    lastNameError.style.display = "none";
+  }
+  if (checkLength(message, 10)) {
+    messageError.style.display = "block";
+  } else {
+    messageError.style.display = "none";
+  }
+}
+
+function checkLength(input, len) {
+  let lengthOfInput = input.value.trim().length;
+  if (lengthOfInput < len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+
+  const patternMatches = regEx.test(email);
+
+  return patternMatches;
+}
