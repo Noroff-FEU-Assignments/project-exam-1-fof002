ee4475bd317da6a5ea1672545bb4ffe3f0364e37
fof002 <80104002+fof002@users.noreply.github.com>
2022-11-20 11:46:27 +0100

    fixed search

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,46 @@
 const navBtn = document.querySelector("#mobile-navigation-btn");
 const mobileNav = document.querySelector("#mobile-menu");
+const urlSearch =
+  "https://frithjof.shop/test/wp-json/wp/v2/posts?per_page=12&_embed";
+const searchInput = document.querySelector("#search-input");
+const searchIcon = document.querySelector("#search-icon");
+const displayedResults = document.querySelector("#displayed-results");
+let searchArray = [];
 
-navBtn.addEventListener("click", () => {
-  mobileNav.classList.toggle("active");
+/*menu*/
+document.addEventListener("click", (event) => {
+  if (
+    event.target.matches("#mobile-navigation-btn") ||
+    event.target.matches("#search-icon")
+  )
+    mobileNav.classList.toggle("active");
   navBtn.classList.toggle("fa-xmark");
 });
 
-/*
+/*search-function*/
 searchInput.addEventListener("keyup", () => {
-  const results = post.filter((result) =>
-    result.title.rendered.startsWith(searchInput.value.toLowerCase())
+  displayedResults.innerHTML = "";
+  const searchValue = searchInput.value.trim().toLowerCase();
+  const searchResults = searchArray.filter((matchingPost) =>
+    matchingPost.title.rendered.toLowerCase().includes(searchValue)
   );
-  console.log(results);
+  if (searchValue.length > 1) {
+    for (i = 0; i < searchResults.length; i++) {
+      displayedResults.innerHTML = "";
+      displayedResults.style.display = "block";
+      let filteredPostTitle = searchResults[i].title.rendered;
+      let filteredPostId = searchResults[i].id;
+      let filteredPostDaste = searchResults[i].date;
+      displayedResults.innerHTML += `<div><a class="link" href="blog-specific.html?id=${filteredPostId}">${filteredPostTitle}</a><span> - (${filteredPostDaste})</span></div>`;
+    }
+  } else {
+    displayedResults.style.display = "none";
+  }
 });
-postArray();
-*/
+
+/*fetching data for search results */
+async function search() {
+  const response = await fetch(urlSearch);
+  searchArray = await response.json();
+}
+search();
